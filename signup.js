dc9dd891c947058bdb8b0c2eeb4d9a8a0dc75204
Hamza Alvi <hamzaalvi224@gmail.com>
2020-04-11 10:20:59 +0500

    add sign up logic

---
+++ signup.js
@@ -0,0 +1,38 @@
+
+const signUpform = document.querySelector(".signUp")
+
+let _signUpInfo = {}
+class SignUpDetail{
+ 
+
+static onSubmitHandler(evt){
+const signUpInfo = {
+  userName: evt.target.elements["user_name"].value,
+  email: evt.target.elements["email"].value,
+  password: evt.target.elements["password"].value,
+  rePassword: evt.target.elements["re-password"].value,
+  ph_number: evt.target.elements["ph_number"].value,
+}
+if(signUpInfo.rePassword !== signUpInfo.password){
+  $(document).ready(function(){
+    $('#pass-modal').modal();
+    $('#pass-modal').modal('open'); 
+ });
+}
+else{
+  _signUpInfo = signUpInfo
+  signUpform.reset()
+  $(document).ready(function(){
+    $('#redi-modal').modal();
+    $('#redi-modal').modal('open'); 
+ });
+}
+}
+
+}
+signUpform.addEventListener("submit",(evt)=>{
+evt.preventDefault()
+  SignUpDetail.onSubmitHandler(evt)
+  
+  
+})
